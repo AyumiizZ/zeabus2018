409ea30c23bd1a0ebe9e2159fb019a02d12f8de6
worachai3 <worachai3@live.com>
2018-03-21 21:26:31 +0700

    mode 0 (find gate)

--- zeabus_command/src/Robosub2018/gate.py
+++ zeabus_command/src/Robosub2018/gate.py
@@ -37,5 +37,36 @@ class Gate(object):
         center = 0
         mode = 0
 
+        while not rospy.is_shutdown() and not mode == -1:
+            # get data from vision service and store in temp variables
+            self.detectGate()
+            area = self.gate_data.area
+            appear = self.gate_data.appear
+            cx = self.gate_data.cx
+            pos = self.data.gate_data.pos
+
+            if mode == 0:
+                print '---mode 0---'
+                # check if gate is appear
+                if appear:
+                    count += 1
+                    reset = 0
+                    print 'FOUND GATE: %d'%(count)
+                elif not appear:
+                    reset += 1
+                    print 'NOT FOUND GATE: %d'%(reset)
+
+                # check counter
+                if count >= 5:
+                    count = 0
+                    reset = 0
+                    print '<<<Change to mode 2>>>'
+                    mode = 2
+                elif reset >= 5:
+                    reset = 0
+                    count = 0
+
+                auv.move('forward', cons.AUV_M_SPEED)
+
 if __name__=='__main__':
     rospy.init_node('gate_node')
