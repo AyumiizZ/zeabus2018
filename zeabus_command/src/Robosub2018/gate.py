6564d0718aef8917f629849b356d25c6768dd21b
worachai3 <worachai3@live.com>
2018-03-21 20:48:20 +0700

    add function for send request to vision service

--- zeabus_command/src/Robosub2018/gate.py
+++ zeabus_command/src/Robosub2018/gate.py
@@ -7,8 +7,19 @@ from zeabus_example.msg import vision_gate
 
 class Gate(object):
     def __init__(self):
+        print '<===INIT GATE===>'
         self.aicontrol = AIControl()
-        
+
+        # wait for vision service and declare variables for store data from vision
+        print '---wait for vision service---'
+        rospy.wait_for_service('vision_gate')
+        self.gate_req = rospy.ServiceProxy('vision_gate', vision_srv_gate)
+        self.gate_data = vision_gate()
+
+    def detectGate(self):
+        # store all data from vision service into gate_data
+        self.gate_data = self.gate_req(String('gate'), String('gate'))
+        self.gate_data = self.gate_data.data
 
 if __name__=='__main__':
     rospy.init_node('gate_node')
