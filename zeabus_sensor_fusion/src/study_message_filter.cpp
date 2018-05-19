aa2d8f3542f3149fe098d1a6df1a08d3c785cd69
Supasan Komonlit <supasan_224@hotmail.com>
2018-05-19 14:41:41 +0700

    succese message filter remove boost::bind

--- zeabus_sensor_fusion/src/study_message_filter.cpp
+++ zeabus_sensor_fusion/src/study_message_filter.cpp
@@ -8,11 +8,11 @@
 
 // include type of any message
 #include	<nav_msgs/Odometry.h>
-#include	<geometry_msgs/Twist.h>
+#include	<geometry_msgs/TwistWithCovarianceStamped.h>
 #include	<sensor_msgs/Imu.h>
 
 void test_sensor_fusion( const nav_msgs::Odometry message_odometry , 
-						 const geometry_msgs::Twist message_twist , 
+						 const geometry_msgs::TwistWithCovarianceStamped message_twist , 
 						 const sensor_msgs::Imu message_orieantation){
 	std::cout << "Test in function call back \n"; 
 }
@@ -24,16 +24,18 @@ int main(int argc , char** argv){
 
 	message_filters::Subscriber< nav_msgs::Odometry> depth_sub(nh , "/baro/odom" , 1);
 	
-	message_filters::Subscriber< geometry_msgs::Twist> velocity_xy_sub(nh , "/dvl/data" , 1);
+	message_filters::Subscriber< geometry_msgs::TwistWithCovarianceStamped> 
+									velocity_xy_sub(nh , "/dvl/data" , 1);
 
 	message_filters::Subscriber< sensor_msgs::Imu> orieantation_sub(nh , "/gx4_45_imu/data" , 1);
 	
 	// The synchronize in c++ can be up to 9 channels
-	message_filters::TimeSynchronizer< nav_msgs::Odometry , geometry_msgs::Twist , 
+	message_filters::TimeSynchronizer< nav_msgs::Odometry 
+										, geometry_msgs::TwistWithCovarianceStamped , 
 										sensor_msgs::Imu > sync( depth_sub , velocity_xy_sub 
 																, orieantation_sub , 10);
 	
-	sync.registerCallback( boost::bind(&test_sensor_fusion, _1, _2 , _3));
+	sync.registerCallback(test_sensor_fusion);
 
 	ros::spin();
 
