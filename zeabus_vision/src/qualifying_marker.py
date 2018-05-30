f207ddc688d1253073b870e988213d4a3e497162
korrawitc <korrawitc@gmail.com>
2018-05-30 11:23:20 +0700

    fix bug (remove some noise)

--- zeabus_vision/src/qualifying_marker.py
+++ zeabus_vision/src/qualifying_marker.py
@@ -81,14 +81,13 @@ def get_object():
 
     lower, upper = get_color("qualifying", "orange", world)
     mask = cv.inRange(hsv, lower, upper)
-    kernel = np.ones((5, 5), dtype=np.uint8)
+    kernel = np.ones((31, 3), dtype=np.uint8)
     mask = cv.GaussianBlur(mask, (5, 5), 0)
     mask = cv.erode(mask, kernel)
     mask = cv.erode(mask, kernel)
     mask = cv.dilate(mask, kernel)
     mask = cv.dilate(mask, kernel)
     return mask
-
 
 def find_marker():
     """
@@ -108,21 +107,32 @@ def find_marker():
     global img, img_res
     while img is None and not rospy.is_shutdown():
         print('img is none.\nPlease check topic name or check camera is running')
-
     himg, wimg = img.shape[:2]
     print (himg, wimg)
+    if wimg != 500:
+        return message()
+
     mask = get_object()
     contours = cv.findContours(
         mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
-    if len(contours) == 0:
-        mode = 1
-    elif len(contours) >= 1:
-        cnt = max(contours, key=cv.contourArea)
+    ROI = []
+    for cnt in contours:
         area = cv.contourArea(cnt)
-        if area < 3000:
-            mode = 1
-        elif area >= 3000:
-            mode = 2
+        if area > 1500:
+            x,y,w,h = cv.boundingRect(cnt)
+            top_excess = (y < 0.05*himg)
+            bot_excess = ((y+h) > 0.95*himg)
+            w_ratio = 0.2
+            if top_excess and bot_excess and 1.0*w/wimg < w_ratio:
+                ROI.append(cnt)
+
+                
+    if len(ROI) == 0:
+        mode = 1
+    elif len(ROI) == 1:
+        mode = 2
+    elif len(ROI) > 1:
+        mode = 3
 
     if mode == 1:
         print_result("MODE 1: CANNOT FIND MARKER")
@@ -131,6 +141,7 @@ def find_marker():
         return message()
     elif mode == 2:
         print_result("MODE 2: CAN FIND MARKER")
+        cnt = ROI[0]
         x, y, w, h = cv.boundingRect(cnt)
         cx_left = x
         cx_right = x+w
@@ -147,6 +158,83 @@ def find_marker():
         publish_result(img, 'bgr', pub_topic + 'img')
         publish_result(mask, 'gray', pub_topic + 'mask')
         return message(cx_left=cx_left, cx_right=cx_right, area=area, appear=True)
+    elif mode == 3:
+        print_result("MODE 3: MAYBE A LOT OF NOISE")
+        cnt = max(ROI,key=cv.contourArea)
+        cx_left = x
+        cx_right = x+w
+        cv.line(img, (cx_left, 0), (cx_left, himg), (255, 0, 0), 1)
+        cv.putText(img, "left", (x+5, himg-30), cv.FONT_HERSHEY_COMPLEX_SMALL, 1,
+                   [0, 0, 0])
+        cv.line(img, (cx_right, 0), (cx_right, himg), (255, 0, 0), 1)
+        cv.putText(img, "right", (x+w+5, himg-30), cv.FONT_HERSHEY_COMPLEX_SMALL, 1,
+                   [0, 0, 0])
+
+        cx_left = Aconvert(cx_left, wimg)
+        cx_right = Aconvert(cx_right, wimg)
+        area = (1.0*area)/(himg*wimg)
+        publish_result(img, 'bgr', pub_topic + 'img')
+        publish_result(mask, 'gray', pub_topic + 'mask')
+        return message(cx_left=cx_left, cx_right=cx_right, area=area, appear=True)
+
+# an old buggy one
+# def find_marker():
+#     """
+#         find marker on the picture draw a line
+#         on left side and right side of marker
+
+#         Returns:
+#             message: (a group of data from vision_path.msg)
+#             if cannot find marker:
+#                 return default value 
+#             else if can find marker:
+#                 return float: cx_left (left side of marker) in range of -1 to 1
+#                        float: cx_right (right side of marker) in range of -1 to 1
+#                        float: area in range of 0 to 1
+#                        bool: appear is true
+#     """
+#     global img, img_res
+#     while img is None and not rospy.is_shutdown():
+#         print('img is none.\nPlease check topic name or check camera is running')
+
+#     himg, wimg = img.shape[:2]
+#     print (himg, wimg)
+#     mask = get_object()
+#     contours = cv.findContours(
+#         mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+#     if len(contours) == 0:
+#         mode = 1
+#     elif len(contours) >= 1:
+#         cnt = max(contours, key=cv.contourArea)
+#         area = cv.contourArea(cnt)
+#         if area < 3000:
+#             mode = 1
+#         elif area >= 3000:
+#             mode = 2
+
+#     if mode == 1:
+#         print_result("MODE 1: CANNOT FIND MARKER")
+#         publish_result(img, 'bgr', pub_topic + 'img')
+#         publish_result(mask, 'gray', pub_topic + 'mask')
+#         return message()
+#     elif mode == 2:
+#         print_result("MODE 2: CAN FIND MARKER")
+#         x, y, w, h = cv.boundingRect(cnt)
+#         cx_left = x
+#         cx_right = x+w
+#         cv.line(img, (cx_left, 0), (cx_left, himg), (255, 0, 0), 1)
+#         cv.putText(img, "left", (x+5, himg-30), cv.FONT_HERSHEY_COMPLEX_SMALL, 1,
+#                    [0, 0, 0])
+#         cv.line(img, (cx_right, 0), (cx_right, himg), (255, 0, 0), 1)
+#         cv.putText(img, "right", (x+w+5, himg-30), cv.FONT_HERSHEY_COMPLEX_SMALL, 1,
+#                    [0, 0, 0])
+
+#         cx_left = Aconvert(cx_left, wimg)
+#         cx_right = Aconvert(cx_right, wimg)
+#         area = (1.0*area)/(himg*wimg)
+#         publish_result(img, 'bgr', pub_topic + 'img')
+#         publish_result(mask, 'gray', pub_topic + 'mask')
+#         return message(cx_left=cx_left, cx_right=cx_right, area=area, appear=True)
 
 
 if __name__ == '__main__':
