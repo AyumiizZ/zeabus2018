2b7c991b7dad1cbdd4608b156273a79c6a44cb36
kam7411 <kamkamkam7-4-11@hotmail.com>
2018-06-02 13:50:21 +0700

    update Khem_path_testing_T_T

--- zeabus_vision/src/Khem_path_testing.py
+++ zeabus_vision/src/Khem_path_testing.py
@@ -3,8 +3,10 @@ import rospy
 import numpy as np
 import rosbag
 import cv2 as cv
+from scipy import stats
+from statistics import mode
 img = None
-bag = rosbag.Bag('01.bag', "r")
+bag = rosbag.Bag('07.bag', "r")
 messages = bag.read_messages(topics="/bottom/left/image_raw/compressed")
 num_image = bag.get_message_count(topic_filters=["/bottom/left/image_raw/compressed"])
 def main() :
@@ -19,25 +21,59 @@ def main() :
         dim = (size, int(img.shape[0] * r))
         resized = cv.resize(img, dim, interpolation = cv.INTER_AREA)
         img = resized
+        cv.imshow('first',img)
         hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
-        #cv.imshow('1_gray',hsv)
-        h , s ,v = cv.split(img)
-        s.fill(100)
-        # v.fill(200)
+        # cv.imshow('1_gray',hsv)
+        h , s ,v = cv.split(hsv)
+        # print s
+        # v.fill(np.mean(s))
+        dark = np.mean(v,dtype='uint64')
+        # print dark
+        # print '---'
+        v = np.array(v) - dark
+        # print s
+        # print '---'
+        # print s - np.mean(s)
+        # # dark.astype(np.uint8)
+        # s.fill(s)
         gray = cv.merge((h,s,v))
-        #cv.imshow('2_gray',gray)
-        gray = cv.cvtColor(img,cv.COLOR_HSV2BGR)
-        gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-        #cv.imshow('3_gray',gray)
+        # h,s,v = cv.split(gray)
+        # print s
+        # cv.imshow('2_gray',gray)
+        gray = cv.cvtColor(gray,cv.COLOR_HSV2BGR)    
+        cv.imshow('second',gray)
+        gray = cv.cvtColor(gray,cv.COLOR_BGR2GRAY)
+        cv.imshow('third',gray)
+        a = np.array(s)
+        # print(" ----------------- I will print a -------------------")
+        # print a
+        m = stats.mode(a)[0]
+        # print(" ----------------- I will print m -------------------")
+        # print m
+        # p = stats.mode(m)[0]
+        # for i in m :
+        # q = mode(m[0])
+        # print(" ----------------- I will print q -------------------")
+        # print q
+        # cv.imshow('3_gray',gray)
         # h , s ,v = cv.split(img)
-        # print np.mean(s)
-        cv.imshow('g',gray)
+        # print s
+        # print np.mean(v)
+        # md = []
+        # mod = []
+        # for c in s :
+        #     for i in stats.mode(c) :
+        #         md.append(i)
+        #         # print md[0][0][0]
+        #         mod.append(stats.mode(md)[0][0][0])
+        # print mode(mod)               
+        # cv.imshow('g',gray)
         # s.fill(150)
         # gray = cv.merge((h,s,v))
-        gray[gray > 240] = 0
-        gray[gray <= 140] = 0
-        # gray[gray > 340- np.mean(s)] = 0
-        # gray[gray <= 200 - np.mean(s)] = 0
+        # gray[gray > np.mean(v)-30] = 0
+        # gray[gray <= np.mean(v)-140] = 0
+        gray[gray > 169] = 0
+        gray[gray <= 77] = 0
         cv.imshow('gray',gray)
         cnt = cv.findContours(gray, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
         cv.drawContours(img,cnt,-1,(0,0,0),3)
@@ -46,14 +82,14 @@ def main() :
             cnt = (sorted(cnt,key=cv.contourArea,reverse=True)[0])
             rect = cv.minAreaRect(cnt)
             area = cv.contourArea(cnt)
-            for c in cnt :
-                peri = cv.arcLength(c, True)
-                approx = cv.approxPolyDP(c, 0.04 * peri, True)
-                if len(approx) == 4:
-                    rectangle = True
+            # for c in cnt :
+            #     peri = cv.arcLength(c, True)
+            #     approx = cv.approxPolyDP(c, 0.04 * peri, True)
+            #     if len(approx) == 4:
+            #         rectangle = True
             # if area > 2000 and rectangle: 
             # rect = cv.minAreaRect(i)
-            if area > 2000 :
+            if area > 1800 :
                 box = cv.boxPoints(rect)
                 box = np.int0(box)
                 free = img.copy()
