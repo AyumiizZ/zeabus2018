508a3b8be6a85e3844600b5b53df76a0d1ea150d
kam7411 <kamkamkam7-4-11@hotmail.com>
2018-06-10 13:03:25 +0700

    init casino gate

--- zeabus_vision/src/casino_gate.py
+++ zeabus_vision/src/casino_gate.py
@@ -9,14 +9,14 @@ import rospy
 import cv2 as cv
 import numpy as np
 from sensor_msgs.msg import CompressedImage, Image
-from zeabus_vision.msg import vision_qualifying_gate
-from zeabus_vision.srv import vision_srv_qualifying_gate
+from zeabus_vision.msg import vision_casino_gate
+from zeabus_vision.srv import vision_srv_casino_gate
 from cv_bridge import CvBridge, CvBridgeError
 from vision_lib import *
 img = None
 img_res = None
 sub_sampling = 1
-pub_topic = "/vision/qualifying_gate/"
+pub_topic = "/vision/casino_gate/"
 world = "real"
 
 def mission_callback(msg):
@@ -30,7 +30,7 @@ def mission_callback(msg):
     task = msg.task.data
 
     print('task:', str(task))
-    if task == 'gate':
+    if task == 'casino_gate':
         return find_gate()
 
 
@@ -50,15 +50,15 @@ def image_callback(msg):
     img_res = img.copy()
 
 
-def message(cx=-1, pos=-1, area=-1, appear=False):
+def message(cx1=-1, cx2=-1, area=-1, appear=False):
     """
         Convert value into a message (from vision_qualifying_gate.msg)
         Returns:
             vision_qualifying_gate (message): a group of value from args
     """
-    m = vision_qualifying_gate()
-    m.cx = cx
-    m.pos = pos
+    m = vision_casino_gate()
+    m.cx1 = cx1
+    m.cx2 = cx2
     m.area = area
     m.appear = appear
     print(m)
@@ -72,21 +72,15 @@ def get_object():
             mask (ONLY GATE AREA)
     """
     global img
-    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-
-    # real world
-    # lower = np.array([0, 0, 0], dtype=np.uint8)
-    # upper = np.array([180, 180, 68], dtype=np.uint8)
-
-    lower,upper = get_color("qualifying","orange",world)
-    mask = cv.inRange(hsv, lower, upper)
-    kernel = np.ones((5, 5), dtype=np.uint8)
-    mask = cv.GaussianBlur(mask, (5, 5), 0)
-    mask = cv.erode(mask, kernel)
-    mask = cv.erode(mask, kernel)
-    mask = cv.dilate(mask, kernel)
-    mask = cv.dilate(mask, kernel)
-    return mask
+    b,g,r = cv.split(img)
+    tc = 100
+    # hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+    canny = cv.Canny(b,tc,3*tc)
+    publish_result(canny, 'gray', pub_topic + 'canny')
+    kernal = cv.getStructuringElement(cv.MORPH_RECT,(10,10))
+    closed = cv.morphologyEx(canny,cv.MORPH_CLOSE,kernal)
+    publish_result(closed, 'gray', pub_topic + 'closed')
+    return closed
 
 
 def get_roi(mask):
@@ -153,93 +147,42 @@ def find_gate():
     global img, img_res
     while img is None and not rospy.is_shutdown():
         print('img is none.\nPlease check topic name or check camera is running')
-
+    area = -1
+    appear = False
     mask = get_object()
-    ROI, left_excess, right_excess, top_excess, bot_excess = get_roi(mask)
-
-    if len(ROI) == 0:
-        mode = 1
-    if len(ROI) == 1:
-        mode = 2
-    if len(ROI) >= 2:
-        mode = 3
-
-    if mode == 1:
-        print_result("MODE 1: CANNOT FIND GATE")
-        publish_result(img_res, 'bgr', pub_topic + 'img')
-        publish_result(mask, 'gray', pub_topic + 'mask')
-        return message()
-    elif mode == 2:
-        himg, wimg = img.shape[:2]
-        x, y, w, h = cv.boundingRect(ROI[0])
-        area = (1.0*h*w)/(himg*wimg)
-        if left_excess is False and right_excess is True:
-            print_result("MODE 2(-1): CAN FIND LEFT GATE")
-            publish_result(img_res, 'bgr', pub_topic + 'img')
-            publish_result(mask, 'gray', pub_topic + 'mask')
-            return message(pos=-1, area=area, appear=True)
-        elif left_excess is True and right_excess is False:
-            print_result("MODE 2(1): CAN FIND RIGHT GATE")
-            publish_result(img_res, 'bgr', pub_topic + 'img')
-            publish_result(mask, 'gray', pub_topic + 'mask')
-            return message(pos=1, area=area, appear=True)
-        elif left_excess is True and right_excess is True:
-            print_result(
-                "MODE 2(0): CAN FIND ALL GATE(GATE IS BIGGER THAN FRAME)")
-            cx = wimg/2
-            cv.line(img_res, (cx, 0), (cx, himg), (255, 0, 0), 1)
-            publish_result(img_res, 'bgr', pub_topic + 'img')
-            publish_result(mask, 'gray', pub_topic + 'mask')
-            return message(cx=cx, pos=0, area=area, appear=True)
-        elif h < 4*w:
-            print (h,w)
-            print_result("MODE 2(0): CAN FIND ALL GATE")
-            cx = (2*x+w)/2
-            cv.line(img_res, (cx, 0), (cx, himg), (255, 0, 0), 1)
-            publish_result(img_res, 'bgr', pub_topic + 'img')
-            publish_result(mask, 'gray', pub_topic + 'mask')
-            return message(cx=cx, pos=0, area=area, appear=True)
-        else:
-            print_result(
-                "MODE 2(-99): CAN FIND PART OF GATE BUT NOT SURE WHICH PART")
-            publish_result(img_res, 'bgr', pub_topic + 'img')
-            publish_result(mask, 'gray', pub_topic + 'mask')
-            return message(pos=-99, area=area, appear=True)
-
-    if mode == 3:
-        himg, wimg = img.shape[:2]
-        cx_horizontal = []
-        cx_vertical = []
-        for cnt in ROI:
-            x, y, w, h = cv.boundingRect(cnt)
-            M = cv.moments(cnt)
-            cx = int(M["m10"]/M["m00"])
-            if h < 4 * w:
-                cx_horizontal.append(cx)
-            else:
-                cx_vertical.append(cx)
-        if len(cx_horizontal) == 2 or len(cx_horizontal) == 1:  # found or found(on water)
-            print_result("MODE 3(1): CAN FIND HORIZONTAL OF GATE")
-            cx = sum(cx_horizontal)/len(cx_horizontal)
-        elif len(cx_vertical) == 2:
-            print_result("MODE 3(2): CAN FIND VERTICAL OF GATE")
-            cx = sum(cx_vertical)/2
-        else:
-            print_result("MODE 3(3): CAN FIND GATE BUT MAYBE A LOT OF NOISE")
-            cx = (sum(cx_vertical)+(sum(cx_horizontal)*3)) / \
-                (len(cx_vertical)+len(cx_horizontal)+3)
-        cv.line(img_res, (cx, 0), (cx, himg), (255, 0, 0), 1)
-        publish_result(img_res, 'bgr', pub_topic + 'img')
-        publish_result(mask, 'gray', pub_topic + 'mask')
-        return message(cx=cx, pos=0, area=-1, appear=True)
-
+    contours = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+    ROI = []
+    # print "c1"
+    # print len(contours)
+    for cnt in contours:
+        x,y,w,h = cv.boundingRect(cnt)
+        if w < 100 or h < 100:
+            continue
+        w_h_ratio = 1.0*w/h
+        # print w_h_ratio
+        # cv.rectangle(img,(x,y),(x+w,y+h),(0,0,255))
+        himg,wimg = img.shape[:2]
+        right_excess = (x+w > 0.95*wimg)
+        left_excess = (x < 0.05*wimg)
+        if not right_excess and not left_excess:
+        # if w_h_ratio <= 2.2 and w_h_ratio >= 1.8:
+            appear = True
+            ROI.append(cnt)
+            cv.rectangle(img,(x,y),(x+w,y+h),(0,0,255),)
+            return_cx1 = x+(w/4)
+            return_cx2 = x+(w*3/4)
+            return_y = y+(h/2)
+            cv.circle(img,(return_x1,return_y),3,(0,0,0),-1)
+            cv.circle(img,(return_x2,return_y),3,(0,0,0),-1)
+            publish_result(img, 'bgr', pub_topic + 'img')
+    return return_cx1,return_cx2,area,appear
 
 if __name__ == '__main__':
-    rospy.init_node('vision_qualifying_gate', anonymous=True)
+    rospy.init_node('vision_casino_gate', anonymous=False)
     image_topic = get_topic("front",world)
     rospy.Subscriber(image_topic, CompressedImage, image_callback)
     print "init_pub_sub"
-    rospy.Service('vision_qualifying_gate',
-                  vision_srv_qualifying_gate(), mission_callback)
+    rospy.Service('vision_casino_gate',
+                  vision_srv_casino_gate(), mission_callback)
     print "init_ser"
     rospy.spin()
