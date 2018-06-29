1b5a818add912a265b897f8f0a770df63a8c1ed4
korrawitc <korrawitc@gmail.com>
2018-06-29 16:18:14 +0700

    create some part of code (not finish)

--- zeabus_vision/src/buy_a_gold_chip.py
+++ zeabus_vision/src/buy_a_gold_chip.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import CompressedImage
 from zeabus_vision.msg import vision_buy_a_gold_chip
 from zeabus_vision.srv import vision_srv_buy_a_gold_chip
 from vision_lib import *
+import color_text
 img = None
 img_res = None
 sub_sampling = 1
@@ -50,49 +51,34 @@ def message(cx=-1, cy=-1, hit=-1, area=-1, appear=False):
     print(m)
     return m
 
-def get_object():
+def get_object(obj):
     global img
-    if world == "real":
-        hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-        lower = np.array([0, 92, 24], dtype=np.uint8)
-        upper = np.array([60, 255, 201], dtype=np.uint8)
-        mask = cv.inRange(hsv, lower, upper)
-    elif world == "sim":
-        lower = np.array([0, 240, 240], dtype=np.uint8)
-        upper = np.array([10, 255, 255], dtype=np.uint8)
-        mask = cv.inRange(img, lower, upper)
+    if obj == "plate":
+        if world == "real":
+            hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+            lower = np.array([0, 92, 24], dtype=np.uint8)
+            upper = np.array([60, 255, 201], dtype=np.uint8)
+            mask = cv.inRange(hsv, lower, upper)
+        elif world == "sim":
+            lower = np.array([0, 240, 240], dtype=np.uint8)
+            upper = np.array([10, 255, 255], dtype=np.uint8)
+            mask = cv.inRange(img, lower, upper)
+    elif obj == "chip":
+        if world == "real":
+            hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+            lower = np.array([17,41,119], dtype=np.uint8)
+            upper = np.array([62, 189, 255], dtype=np.uint8)
+            mask = cv.inRange(hsv, lower, upper)
+        # if world == "real":
+        #     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        #     lower = np.array([0, 92, 24], dtype=np.uint8)
+        #     upper = np.array([60, 255, 201], dtype=np.uint8)
+        #     mask = cv.inRange(hsv, lower, upper)
+        elif world == "sim":
+            lower = np.array([0, 240, 240], dtype=np.uint8)
+            upper = np.array([10, 255, 255], dtype=np.uint8)
+            mask = cv.inRange(img, lower, upper) 
     return mask
-
-# def get_cx() :
-#     global img
-#     himg,wimg = img.shape[:2]
-#     ROI_cx = -1
-#     ROI_cy = -1
-#     area = -1
-#     appear = False
-#     hit = 0
-#     cnt = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
-#     for i in cnt :
-#         hit += cv.contourArea(i) 
-#     if cnt > 1 :
-#         cnt = max(cnt,key=cv.contourArea)
-#         area = cv.contourArea(cnt)
-#         if area > 500 :
-#             x,y,w,h = cv.boundingRect(cnt)
-#             cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-#             cx = x + (w/2)
-#             cy = y + (h/2)
-#             top_excess = (y < 0.05*himg)
-#             bot_excess = ((y+h) > 0.95*himg)
-#             right_excess = (x+w > 0.95*wimg)
-#             left_excess = (x < 0.05*wimg)
-#             w_h_ratio = 1.0*w/h
-#             window_excess = top_excess or bot_excess or right_excess or left_excess
-#             if (not window_excess) and w_h_ratio >= 0.5 and w_h_ratio < 2:
-#                 appear = True
-#                 cv.circle(img_res, (cx, cy), 2, (0, 0, 255), -1)
-#                 return cx , cy , area , hit , appear 
-#     return cx , cy , area , hit , appear
 
 
 def get_ROI(mask):
@@ -132,21 +118,43 @@ def get_cx(cnt):
     radius = int(radius)
     cv.circle(img_res, center, radius, (255, 0, 0), 2)
     cx = Aconvert(cx,wimg)
-    # 1.0*(cx - (wimg/2))/(1.0*wimg/2)
     cy = -1.0*Aconvert(cy,himg)
-    # (cy - (himg/2))/(1.0*himg/2)
     area = (1.0*w*h)/(wimg*himg)
     return cx,cy,area
 
 def find_chip():
-    pass
+    global img, img_res
+    while img is None and not rospy.is_shutdown():
+        img_is_none()
+    mask = get_object("chip")
+    _,ROI = get_ROI(mask)
+
+    if ROI == []:
+        mode = 1
+    elif len(ROI) == 1:
+        mode = 2
+        chip = ROI[0]
+    elif len(ROI) > 1:
+        mode = 3
+        chip = max(ROI, key=cv.contourArea)
+    
+    if mode == 1:
+        print_result("MODE 1: NOT FOUND",color_text.RED)
+        publish_result(img_res, 'bgr', pub_topic + 'img_res')
+        publish_result(mask, 'gray', pub_topic + 'mask')
+        return message()
+    elif mode == 2:
+        print_result("MODE 2: FOUND A GOLD CHIP",color_text.GREEN)
+    elif mode == 3:
+        print_result("MODE 3: FOUND BUT HAVE SOME NOISE",color_text.YELLOW)
+
 
 def find_plate():
     global img, img_res
     while img is None and not rospy.is_shutdown():
         print('img is none.\nPlease check topic name or check camera is running')
 
-    mask = get_object()
+    mask = get_object("plate")
     hit, ROI = get_ROI(mask)
     if ROI == []:
         mode = 1
@@ -166,46 +174,20 @@ def find_plate():
         publish_result(img_res, 'bgr', pub_topic + 'img_res')
         publish_result(mask, 'gray', pub_topic + 'mask')
         return message(cx=cx,cy=cy,hit=hit,area=area,appear=True)
-# def find_plate() :
-#     global img, img_res
-#     while img is None and not rospy.is_shutdown():
-#         print('img is none.\nPlease check topic name or check camera is running')
-
-#     mask = get_object()
-#     cx, cy, area ,hit, appear = get_cx(mask)
-#     if cx == -1 and cy == -1 :
-#         mode = 1
-#     elif cx != -1 and cy != -1:
-#         mode = 2
-#     if mode == 1:
-#         print_result("MODE 1: CANNOT FIND PLATE")
-#         publish_result(img_res, 'bgr', pub_topic + 'img_res')
-#         publish_result(mask, 'gray', pub_topic + 'mask')
-#         return message()
-#     elif mode == 2:
-#         print_result("MODE 2: CAN FIND 1 CX AND 1 CY")
-#         himg, wimg = img.shape[:2]
-#         return_cx = Aconvert(cx,wimg)
-#         return_cy = Aconvert(cy,himg)
-#         return_area = (1.0*area)/(himg*wimg)
-#         return_his = hit/(himg*wimg)
-#         publish_result(img_res, 'bgr', pub_topic + 'img_res')
-#         publish_result(mask, 'gray', pub_topic + 'mask')
-#         return message(cx=return_cx, cy=return_cy, hit = return_hit , area=return_area, appear=True)
 
 if __name__ == '__main__':
     rospy.init_node('vision_buy_a_gold_chip', anonymous=False)
-    print_result("INIT NODE")
+    print_result("INIT NODE",color_text.GREEN)
     image_topic = get_topic("front",world)
     rospy.Subscriber(image_topic, CompressedImage, image_callback)
-    # image_topic = get_topic("bottom",world)
-    # rospy.Subscriber(image_topic, CompressedImage, image_callback)
-    print_result("INIT SUBSCRIBER")
+    image_topic = get_topic("bottom",world)
+    rospy.Subscriber(image_topic, CompressedImage, image_callback)
+    print_result("INIT SUBSCRIBER",color_text.GREEN)
     rospy.Service('vision_buy_a_gold_chip', vision_srv_buy_a_gold_chip(),
                   mission_callback)
-    print_result("INIT SERVICE")
+    print_result("INIT SERVICE",color_text.GREEN)
     rospy.spin()
-    print_result("END PROGRAM")
+    print_result("END PROGRAM",color_text.RED+color_text.YELLOW_HL)
     # while True and not rospy.is_shutdown():
     #     find_plate()
 
