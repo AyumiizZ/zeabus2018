5531bfc383d1f1d64f6e419e960c08bf1319df9f
skconan <supakit.kr@gmail.com>
2018-07-06 00:53:53 +0800

    draft method

--- zeabus_command/src/shoot_craps.py
+++ zeabus_command/src/shoot_craps.py
@@ -2,135 +2,64 @@
 
 import rospy
 from aicontrol import AIControl
-from zeabus_vision.msg import vision_dice
-from zeabus_vision.srv import vision_srv_dice
+from std_msgs.msg import String
+from aicontrol import AIControl
+from zeabus_vision.msg import vision_shoot_craps
+from zeabus_vision.srv import vision_srv_shoot_craps
 
-class dice(object):
+'''
+    Use dice in singular and plural
+'''
+
+
+class ShootCraps(object):
     def __init__(self):
         self.aicontrol = AIControl()
-        self.data = vision_dice
-        rospy.wait_for_service('vision_dice')
-        self.detect_dice = rospy.ServiceProxy('vision_dice', vision_srv_dice)
+        rospy.wait_for_service('vision_shoot_craps')
+        self.detect_dice = rospy.ServiceProxy(
+            'vision_dice', vision_srv_shoot_craps)
 
-    def detectDie(self, point) :
-        self.data = self.detect_dice(String('dice'), point)
+    def detectDie(self, point):
+        self.data = self.detect_dice(String('shoot_craps'), point)
         self.data = self.data.data
 
-    def nextDie(self, cur):
-        if cur == 1:
-            return 6
-        elif cur == 2:
-            return 5
-        elif cur == 5:
-            return 6
-        elif cur == 6:
-            return 5
+    def isHit(self):
+        pass
+
+    '''
+        Find all dice for set the checkpoint
+    '''
+
+    def detectAllDice(self):
+        pass
+
+    '''
+        Find dice 5 or 6
+    '''
+
+    def detectDice(self):
+        pass
+
+    '''
+        back to check point 
+    '''
+
+    def back2CheckPoint(self):
+        pass
+
+    '''
+        Predict dice position by previous position (Forwarding) 
+        if cannot detect dice more than X frame
+    '''
+
+    def predictDicePosition(self):
+        pass
 
     def run(self):
-        mode = 0
-        count = 0
-        reset = 0
+        pass
 
-        target_die = 0
-        appear = False
 
-        checkpoint = [0, 0]
-
-        count_dice = [0, 0, 0, 0]
-        reset_count = [0, 0, 0, 0]
-
-        dice = {0:1, 1:2, 2:5, 3:6}
-
-        while not rospy.is_shutdown() and mode != -1:
-            if mode == 0:
-                print 'Mode 0'
-                for index in dice:
-                    auv.move('forward', cons.AUV_M_SPEED)
-                    print 'Look for %d point'%dice[index]
-                    self.detectDie(dice[index])
-                    appear = self.data.appear
-                    if appear:
-                        count_dice[index] += 1
-                        reset_count[index] = 0
-                    else:
-                        reset_count[index] += 1
-
-                    if reset_count[index] >= 5:
-                        count_dice[index] = 0
-                        reset_count[index] = 0
-
-                    if count_dice[index] >= 5:
-                        target_die = dice[index]
-                        print 'appear %d point'%(target_die)
-                        print 'Set target to %d'%(target_die)
-                        checkpoint = [auv.auv_state[0], auv.auv_state[1]]
-                        print '---checkpoint---'
-                        print checkpoint
-                        mode = 1
-                        break
-
-            if mode == 1:
-                print 'Mode 1'
-                self.detectDie(target_die)
-                appear = self.data.appear
-                cx = self.data.cx
-                cy = self.data.cy
-                print '<<<Data from Vision>>>'
-                print 'APPEAR: %s'%(appear)
-                print 'CX: %f'%(cx)
-                print 'CY: %f'%(cy)
-                if appear:
-                    if -cons.VISION_DICE_ERROR < cx < cons.VISION_DICE_ERROR:
-                        if -cons.VISION_DICE_ERROR < cy < cons.VISION_DICE_ERROR:
-                            auv.multiMove([1, cx, cy, 0, 0, 0])
-                    else:
-                        auv.multiMove([0, cx, cy, 0, 0, 0])
-                    reset += 1
-                else:
-                    count += 1
-                    reset = 0
-
-                if count >= 5:
-                    target_die = nextDie(target_die)
-                    print 'next target is %d'%(target_die)
-                    auv.backTrack(checkpoint[0], checkpoint[1])
-                    mode = 2
-                if reset >= 5:
-                    count = 0
-                    reset = 0
-
-            if mode == 2:
-                self.detectDie(target_die)
-                appear = self.data.appear
-                cx = self.data.cx
-                cy = self.data.cy
-                print '<<<Data from Vision>>>'
-                print 'APPEAR: %s'%(appear)
-                print 'CX: %f'%(cx)
-                print 'CY: %f'%(cy)
-                if appear:
-                    if -cons.VISION_DICE_ERROR < cx < cons.VISION_DICE_ERROR:
-                        if -cons.VISION_DICE_ERROR < cy < cons.VISION_DICE_ERROR:
-                            auv.multiMove([1, cx, cy, 0, 0, 0])
-                    else:
-                        auv.multiMove([0, cx, cy, 0, 0, 0])
-                    reset += 1
-                else:
-                    count += 1
-                    reset = 0
-
-                if count >= 5:
-                    mode = 3
-                if reset >= 5:
-                    count = 0
-                    reset = 0
-
-            if mode == 3:
-                auv.driveX(-4)
-                auv.backTrack(checkpoint[0], checkpoint[1])
-                auv.depthAbs(-1)
-                auv.driveX(5)
-
-if __name__=='__main__':
+if __name__ == '__main__':
     rospy.init_node('shoot_craps')
-    dice = dice()
+    SC = ShootCraps()
+    SC.run()
