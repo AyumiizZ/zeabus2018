4eef3e7e1d7689c38c732b2c258aa239124640d0
skconan <supakit.kr@gmail.com>
2018-07-06 16:22:32 +0800

    created timer

--- zeabus_command/src/shoot_craps.py
+++ zeabus_command/src/shoot_craps.py
@@ -6,21 +6,43 @@ from std_msgs.msg import String
 from aicontrol import AIControl
 from zeabus_vision.msg import vision_shoot_craps
 from zeabus_vision.srv import vision_srv_shoot_craps
+import time
 
 '''
     Use dice in singular and plural
 '''
 
+class Timer(object):
+    def __init__(self):
+        self.beginTime = 0
+        self.timeout = 0
+
+    def setTimeout(self, sec):
+        self.beginTime = 0
+        self.timeout = sec
+        self.beginTime = time.time()
+
+    def isTimeout(self):
+        if time.time() - self.beginTime >= self.timeout:
+            return True
+        else:
+            return False
 
 class ShootCraps(object):
     def __init__(self):
-        self.aicontrol = AIControl()
+        print("SHOOT CRAPS")
+        # self.aicontrol = AIControl()
+        print('Wait for service...')
         rospy.wait_for_service('vision_shoot_craps')
+        print('Service Already')
+        self.data = None
+        self.checkPoint = [0,0]
         self.detect_dice = rospy.ServiceProxy(
-            'vision_dice', vision_srv_shoot_craps)
+            'vision_shoot_craps', vision_srv_shoot_craps)
+        self.timer = Timer()
 
-    def detectDie(self, point):
-        self.data = self.detect_dice(String('shoot_craps'), point)
+    def request(self):#, point):
+        self.data = self.detect_dice(String('shoot_craps'))#, point)
         self.data = self.data.data
 
     def isHit(self):
@@ -28,10 +50,20 @@ class ShootCraps(object):
 
     '''
         Find all dice for set the checkpoint
+        Condition
+            - Low speed
+            - Cener between 5 and 6
+        Break
+            - Distance more than X
+
     '''
 
     def detectAllDice(self):
-        pass
+        self.timer.setTimeout(10)
+
+        while not self.timer.isTimeout():
+            self.request()
+        print('Timeout')
 
     '''
         Find dice 5 or 6
@@ -56,10 +88,13 @@ class ShootCraps(object):
         pass
 
     def run(self):
+        print('SHOOT CRAPS RUNNING')
+        self.detectAllDice()
         pass
 
 
 if __name__ == '__main__':
-    rospy.init_node('shoot_craps')
+    rospy.init_node('shoot_craps', anonymous=False)
+    print("INIT NODE")
     SC = ShootCraps()
     SC.run()
