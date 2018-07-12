401e5d458a52ef0b73facd1a8da600ea7e5608d6
Supasan Komonlit <supasan_224@hotmail.com>
2018-07-13 00:17:30 +0700

    Init filter pressure sensor

--- zeabus_sensor_fusion/src/specific_filter/pressure_sensor.cpp
+++ zeabus_sensor_fusion/src/specific_filter/pressure_sensor.cpp
@@ -1,9 +1,12 @@
-/* --------------------- this file will plan to filter pressure sensor ------------------------
+/* 
+    --------------------- this file will plan to filter pressure sensor ------------------------
 		by use if else and use distance you have to ok or get new value when have more distance
 */
 
 
 #include	<iostream>
+
+#include	<ctime>
 
 #include	<ros/ros.h>
 #include	<ros/package.h>
@@ -20,7 +23,8 @@ void listen_baro_odom( const nav_msgs::Odometry message_odometry);
 void absolute_detect( double diff , double preblem);
 
 // declare variable for pressure_sensor
-double previous_data , ok_data , new_data;
+double ok_data , new_data;
+bool get_new_value;
 
 int main( int argc , char** argv){
 	ros::init( argc , argv , "filter_pressure");
@@ -29,28 +33,62 @@ int main( int argc , char** argv){
 
 	// init variable
 	std::string input_topic = "/baro/odom";
-	int input_size = 5;
+	int input_size = 5; // queue for subscriber
+
 	std::string output_topic = "/pressure_sensor";
-	int output_size = 5;
-	double frequency_time = 100;
-	double limit_value = 40;
-	double ok_different = 0.5;
+	int output_size = 5; // queue for publisher
+
+	int frequency_time = 100;
+
+	int count = 0;
+	int limit_value = 40; // limit value will ok new value
+	double ok_different = 0.5; // ok different to use 
+
+	std::time_t offset_time = std::time(NULL); // last time have recieve data from topic
+	std::time_t limit_time = 10; // limit value time have to warning not receive data
+	std::time_t now_time = std::time(NULL); // now time
+	get_new_value = false;
+
+	ros::Rate rate(frequency_time);
 
 	// declare about subscriber and publisher
 	ros::Subscriber sub_baro_odom = nh.subscribe( input_topic , input_size , &listen_baro_odom);
 	ros::Publisher tell_baro_odom = nh.advertise< nav_msgs::Odometry>
 											( output_topic , output_size);
 
+	// declare about rate of while or rate for calculating
 	std::cout << "input topic is " << input_topic << "\n";
 	std::cout << "input size is " << input_size << "\n";
 	std::cout << "output topic is " << output_topic << "\n";
 	std::cout << "output size is " << output_size << "\n"; 
 	std::cout << "frequency time is " << frequency_time << "\n";
 	std::cout << "ok different is " << ok_different << "\n";
-	
+	std::coit << "rate time is " << frequency_time << "\n";
+
 	while( nh.ok() ){
-		
+		if( get_new_value ){
+			if( absolute_detect( ok_different , ok_data - new_data ) ){
+				count = 0;
+				ok_data = new_data;
+			}
+			offset_time = std::time(NULL);
+			get_new_value = false;
+		}
+		else{
+			now_time = std::time(NULL);
+			if( now_time - offset_time > limit_time){
+				ROS_FATAL(" Warning receive ok data pressure very slow over limit_time");
+			}
+		}
+		rate.sleep();
+		ros::spincOnce();
 	}
+
+}
+
+void listen_baro_odom( const nav_msgs::Odometry message_odometry){
+	new_data = message_odometry.PoseWithCovariance.pose.position.z;
+	get_new_value = true;
 }
 
 bool absolute_detect(double diff , double problem){
