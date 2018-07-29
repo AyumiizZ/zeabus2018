7f022e7b76540878328976384f76238fbda47be0
kam7411 <kamkamkam7-4-11@hotmail.com>
2018-07-29 10:12:56 +0700

    update buy a gold chip

--- zeabus_vision/src/call_service.py
+++ zeabus_vision/src/call_service.py
@@ -7,23 +7,24 @@ from std_msgs.msg import String
 
 if __name__ == '__main__':
     rospy.init_node('call_service')
-    serviceName = 'vision_path'
+    serviceName = 'vision_buy_a_gold_chip'
+    # serviceName = 'vision_path'
     # serviceName = 'vision_navigate'
     # serviceName = 'vision_squid'
     # binsrv = 'vision_bin'
     print('wait service')
     rospy.wait_for_service(serviceName)
     print('service start')
-
+    call = rospy.ServiceProxy(serviceName, vision_srv_buy_a_gold_chip)
     # call = rospy.ServiceProxy(serviceName, vision_srv_navigate)
-    call = rospy.ServiceProxy(serviceName, vision_srv_path)
+    # call = rospy.ServiceProxy(serviceName, vision_srv_path)
     # call = rospy.ServiceProxy(serviceName, vision_srv_default)
     while not rospy.is_shutdown():
         # res = call(String('Navigate'), String('bot'))
-
+        res = call(String('buy_a_gold_chip'), String('front'))
         # res = call(String('squid'), String('b'))
         # res = call(String('bouy'), String('a'))
-        res = call(String('path'))
+        # res = call(String('path'))
         # res = call(String('bouy'), String('g'))
         # res = res.data
         # print res.x
